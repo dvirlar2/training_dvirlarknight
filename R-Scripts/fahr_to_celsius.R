e42708f1e3bcff7d02e98161638566234b91e837
Daphne Virlar-Knight <virlar-knight@nceas.ucsb.edu>
2022-02-17 10:22:39 -0800

    Added ROxygen Skeleton to function script

--- R-Scripts/fahr_to_celsius.R
+++ R-Scripts/fahr_to_celsius.R
@@ -1,13 +1,15 @@
-airtemps <- c(212, 30.3, 78, 32)
-
-celsius1 <- (airtemps[1]-32)*5/9
-celsius2 <- (airtemps[2]-32)*5/9
-celsius3 <- (airtemps[3]-32)*5/9
-
+#' Fahrenheit to Celsius
+#' 
+#'
+#' @param fahr Temperature in Fahrenheit as a vector
+#'
+#' @return Temperature in Celsius
+#' @export
+#'
+#' @examples
+#' fahr_to_celsius(c(212, 32))
 fahr_to_celsius <- function(fahr){
   celsius <- (fahr - 32)*5/9
   return(celsius) # this basically prints the output if you run the fx w/o
-  # assigning it to an object
+  # assigning it to an object, and calling the object
 }
-
-fahr_to_celsius(airtemps)
